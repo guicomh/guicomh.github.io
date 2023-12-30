45dcc1aba66a5717d300d31a0a5e2887c426633d
Guilherme <eliseuvasconcellos@gmail.com>
2023-12-30 11:28:16 -0300

    update all

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-
 document.getElementById("meuBotao").addEventListener("click", function() {
     window.location.href = "https://www.linkedin.com/in/guilherme-vasconcellos-176736270/";
+});
+
+document.getElementById("seuBotao").addEventListener("click", function() {
+    window.location.href = "https://www.jaguanumlixozero.com.br";
 });
