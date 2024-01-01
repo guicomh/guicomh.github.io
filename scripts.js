2705e0813dc046f4f258ac46c8c80d741ddf14f8
Guilherme <eliseuvasconcellos@gmail.com>
2024-01-01 20:31:38 -0300

    update novo

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("meuBotao").addEventListener("click", function() {
-    window.location.href = "https://github.com/guicomh";
-});
-
-document.getElementById("seuBotao").addEventListener("click", function() {
-    window.location.href = "https://www.jaguanumlixozero.com.br";
-});
