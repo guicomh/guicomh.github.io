1350cd39eb279f46a043482622b1463f15ae4812
Guilherme <eliseuvasconcellos@gmail.com>
2024-01-08 17:45:16 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 document.getElementById("botão-magico").addEventListener("click", function() {
-    window.location.href = "https://jaganumlixozero.com.br/";
+    window.location.href = "https://jaguanumlixozero.com.br/";
 });
+
+document.getElementById("botão-site").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/ogguizinhu/";
+});
+
